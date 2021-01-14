2ae96aea10450e6a6693ecb5a2f49fdc7023fc2f
Simon Waight <simon.waight@microsoft.com>
2021-01-14 00:22:40 +0000

    App application insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("baf10c87-6553-4079-a53b-02f3cd926060");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
